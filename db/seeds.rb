7ddbc475720e95172148bd13a08c3487fdf8c204
Taylor <tdaily0196@gmail.com>
2022-04-28 17:21:18 -0400

    Create user and messagae models, prep for displaying messages from database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.find(1)
+Message.create(body: "Message 1",user: user)
+user = User.find(2)
+Message.create(body: "Message 2",user: user)
+user = User.find(3)
+Message.create(body: "Message 3",user: user)
+user = User.find(4)
+Message.create(body: "Message 4",user: user)
+user = User.find(5)
+Message.create(body: "Message 5",user: user)
